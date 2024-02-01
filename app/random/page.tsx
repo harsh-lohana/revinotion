46823a1696b6c2209a51eaba253900310812c4bf
Harsh Lohana <harshylohana15@gmail.com>
2024-02-01 13:14:41 +0530

    client api access fix

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -7,27 +7,27 @@ const Random = () => {
   const [loading, setLoading] = useState<any>(false);
   const [randomQuestion, setRandomQusetion] = useState<any>();
 
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const resq = await fetch("/api/random", { cache: "no-store" });
-      const op = await resq.json();
-      setRandomQusetion(op.random);
-      setLoading(false);
-    })();
-    return () => {};
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     setLoading(true);
+  //     const resq = await fetch("/api/random", { cache: "no-store" });
+  //     const op = await resq.json();
+  //     setRandomQusetion(op.random);
+  //     setLoading(false);
+  //   })();
+  //   return () => {};
+  // }, []);
 
   return (
     <main className="flex flex-col h-screen items-center">
       <h1 className="text-4xl">Random</h1>
-      {randomQuestion && (
+      {/* {randomQuestion && (
         <div className="flex flex-col">
           <Link href={randomQuestion.url}>
             <h1>{randomQuestion.name}</h1>
           </Link>
         </div>
-      )}
+      )} */}
     </main>
   );
 };
