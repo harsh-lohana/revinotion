fb667e401b29f9e38cea2265c000f6748c7a9c59
Harsh Lohana <harshylohana15@gmail.com>
2024-03-20 09:35:57 +0530

    cache removed

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/api/random',
+                source: '/api',
                 headers: [
                     {
                         key: 'Cache-Control',
