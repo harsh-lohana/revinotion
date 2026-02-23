1cc7030913ea5d3d487a701cb92f33d6dbbb52a9
Harsh Lohana <harshylohana15@gmail.com>
2026-02-23 23:24:48 +0530

    cleanup

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <main className="flex min-h-screen flex-col justify-center items-center">
-      <SignIn />
-    </main>
-  );
-}
--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <main className="flex min-h-screen flex-col justify-center items-center">
-      <SignUp />
-    </main>
-  );
-}
